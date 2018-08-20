a8cfcc7dccfb3539a7086f951c764736110dd876
pacoard <pacoard@gmail.com>
2018-08-19 21:13:19 -0500

    Test progress

--- test/ticket_ownership.test.js
+++ test/ticket_ownership.test.js
@@ -1,16 +1,63 @@
 // https://gist.github.com/cipherzzz/fe4703c27e79c31b345870aafdd98391#file-erc721-spec-js
 
+const TicketOwnership = artifacts.require("./TicketOwnership.sol") //need ownership methods for test purposes
+const tryCatch = require("./exceptions.js").tryCatch //for testing reverts
+const errTypes = require("./exceptions.js").errTypes
+
+function getTicketIDs(bns) {
+	for (i = 0; i < bns.length; i++) {
+		bns[i] = bns[i].toString().split('e+')[0].replace('.', '')
+	}
+	return bns
+}
+
+contract('TicketOwnership', function(accounts) {
+	// accounts
+	const OWNER = accounts[0]
+	const ALICE = accounts[1]
+	const BOB = accounts[2]
+
+	// contract to interact with
+	let t_ownership
+
+	beforeEach('setup contract for each test', async function () {
+        t_ownership = await TicketOwnership.deployed()
+		await t_ownership.startSale({from: OWNER})
+
+		// buyTicket method tested in ticket_sale.test.js
+		await t_ownership.buyTicket( 2, {value: 2e+18, from: ALICE})
+		await t_ownership.buyTicket( 1, {value: 1e+18, from: BOB})
+    })
+
+	// test ownerOf and balanceOf
 	it("should get owner by ticket and balance of owners", async () => {
-		// test ownerOf and balanceOf
 
-	});
-
-	it("should properly handle approvals", async () => {
-		// test approvals
-
-	});
-
-	it("should properly handle token transfers", async () => {
-		// test transfer of tokens
+		let aliceTickets = getTicketIDs(await t_ownership.ticketsOf(ALICE))
+		let bobTickets = getTicketIDs(await t_ownership.ticketsOf(BOB))
 		
-	});
+		let aliceBalance = await t_ownership.balanceOf(ALICE)
+		let bobBalance = await t_ownership.balanceOf(BOB)
+
+		// test balanceOf
+		assert.equal(aliceTickets.length, aliceBalance, 'check balanceOf method')
+		assert.equal(bobTickets.length, bobBalance, 'check balanceOf method')
+
+		// test ownerOf
+		assert.equal(await t_ownership.ownerOf(aliceTickets[0]), ALICE, 'check ownerOf')
+		assert.equal(await t_ownership.ownerOf(aliceTickets[1]), ALICE, 'check ownerOf')
+		assert.equal(await t_ownership.ownerOf(bobTickets[0]), BOB, 'check ownerOf')
+
+	})
+
+	// test approvals
+	it("should properly handle approvals", async () => {
+
+
+	})
+
+	// test transfer of tokens
+	it("should properly handle token transfers", async () => {
+
+	})
+
+})
--- test/ticket_sale.test.js
+++ test/ticket_sale.test.js
@@ -17,132 +17,136 @@ contract('TicketSale', function(accounts) {
 	const MAX_TICKETS_PER_PERSON = 8
 	const TICKET_PRICE = web3.toWei(1, "ether")
 
-	/*it("should change state variables of the contract", async () => {
+	// contract to interact with
+	let t_sale
+
+	beforeEach('setup contract for each test', async function () {
+		t_sale = await TicketSale.deployed()
+	})
+
+	it("should change state variables of the contract", async () => {
 		// setIPFSdata
 		// addMoreTickets
 		// setTicketPrice
-
-		let ticketsale = await TicketSale.deployed()
-
+/*
 		// Check initial constructor parameters (max tickets per person already tested in the following test)
-		let curr_name = await ticketsale.name.call()
+		let curr_name = await t_sale.name()
 		assert.equal(curr_name, NAME, 'initial name passed to constructor is wrong')
-		let curr_ipfshash = await ticketsale.ipfsMetaData.call()
+		let curr_ipfshash = await t_sale.ipfsMetaData()
 		assert.equal(curr_ipfshash, IPFS_HASH, 'initial IPFS hash passed to constructor is wrong')
-		let curr_maxtickets = await ticketsale.maxTickets.call()
+		let curr_maxtickets = await t_sale.maxTickets()
 		assert.equal(curr_maxtickets.toNumber(), MAX_TICKETS, 'initial max tickets number passed to constructor is wrong')
-		let curr_ticketprice = await ticketsale.ticketPrice.call()
+		let curr_ticketprice = await t_sale.ticketPrice()
 		assert.equal(curr_ticketprice.toNumber(), TICKET_PRICE, 'initial ticket price passed to constructor is wrong')
 
 		// Check setter methods (should be accessible only by the owner)
 
 		// SET IPFS META DATA
 		await tryCatch(
-        	ticketsale.setIpfsMetaData.call("new hash", {from: ALICE}), errTypes.revert
+        	t_sale.setIpfsMetaData("new hash", {from: ALICE}), errTypes.revert
         )
-		await ticketsale.setIpfsMetaData.call("new hash", {from: OWNER})
+		await t_sale.setIpfsMetaData("new hash", {from: OWNER})
 		
 
 		// ADD MORE TICKETS
 		await tryCatch(
-        	ticketsale.addMoreTickets.call(10, {from: ALICE}), errTypes.revert
+        	t_sale.addMoreTickets(10, {from: ALICE}), errTypes.revert
         )
-		await ticketsale.addMoreTickets.call(10, {from: OWNER})
+		await t_sale.addMoreTickets(10, {from: OWNER})
 		
 
 		// CHANGE TICKET PRICE
 		await tryCatch(
-        	ticketsale.setTicketPrice.call(web3.toWei(3, "ether"), {from: ALICE}), errTypes.revert
+        	t_sale.setTicketPrice(web3.toWei(3, "ether"), {from: ALICE}), errTypes.revert
         )
-		await ticketsale.setTicketPrice.call(web3.toWei(3, "ether"), {from: OWNER})
+		await t_sale.setTicketPrice(web3.toWei(3, "ether"), {from: OWNER})
 		
 
 
-		curr_ipfshash = await ticketsale.ipfsMetaData.call()
-		assert.equal(await ticketsale.ipfsMetaData.call(), "new hash", 'check setIpfsMetaData method')
-		curr_maxtickets = await ticketsale.maxTickets.call()
+		curr_ipfshash = await t_sale.ipfsMetaData()
+		assert.equal(await t_sale.ipfsMetaData(), "new hash", 'check setIpfsMetaData method')
+		curr_maxtickets = await t_sale.maxTickets()
 		assert.equal(curr_maxtickets.toNumber(), MAX_TICKETS + 10, 'check addMoreTickets method')
-		curr_ticketprice = await ticketsale.ticketPrice.call()
+		curr_ticketprice = await t_sale.ticketPrice()
 		assert.equal(curr_ticketprice.toNumber().toString(), web3.toWei(3, "ether"), 'check setTicketPrice method')
-
-	});*/
+*/
+	})
 
 
 	it("should manage normal and edge cases of buying tickets", async () => {
-		let ticketsale = await TicketSale.deployed()
 		// State of the contract should be "created" so it should not be possible to buy a ticket
         /*assert*/await tryCatch(
-        	ticketsale.buyTicket( 2, {value: 2e+18, from: ALICE}), errTypes.revert
+        	t_sale.buyTicket( 2, {value: 2e+18, from: ALICE}), errTypes.revert
         )
 
         // Start sale, emits event
-		await ticketsale.startSale({from: OWNER})
+		await t_sale.startSale({from: OWNER})
 		
 		// Alice tries to buy 2 tickets with little money
 		/*assert*/await tryCatch(
-			ticketsale.buyTicket( 2, {value: 1e+18, from: ALICE}), errTypes.revert
+			t_sale.buyTicket( 2, {value: 1e+18, from: ALICE}), errTypes.revert
 		)
 		
 		// Alice tries to buy more tickets than allowed per person
 		/*assert*/await tryCatch(
-			ticketsale.buyTicket( MAX_TICKETS_PER_PERSON + 1, {value: MAX_TICKETS_PER_PERSON*1e+18, from: ALICE}), errTypes.revert
+			t_sale.buyTicket( MAX_TICKETS_PER_PERSON + 1, {value: MAX_TICKETS_PER_PERSON*1e+18, from: ALICE}), errTypes.revert
 		)
 		
 		// Alice buys 2 tickets with the right amount of money
- 		await ticketsale.buyTicket( 2, {value: 2e+18, from: ALICE})
-		aliceBalance = await ticketsale.balanceOf.call(ALICE)
+ 		await t_sale.buyTicket( 2, {value: 2e+18, from: ALICE})
+		aliceBalance = await t_sale.balanceOf(ALICE)
 		assert.equal(aliceBalance.toNumber(), 2, 'ticket balance incorrect, check buyTicket method')
 
 		// Alice tries again to buy more tickets than allowed, now that she has two
 		/*assert*/await tryCatch(
-			ticketsale.buyTicket( MAX_TICKETS_PER_PERSON, {value: MAX_TICKETS_PER_PERSON*1e+18, from: ALICE}), errTypes.revert
+			t_sale.buyTicket( MAX_TICKETS_PER_PERSON, {value: MAX_TICKETS_PER_PERSON*1e+18, from: ALICE}), errTypes.revert
 		)
 
 		// Sale is stopped
-		await ticketsale.stopSale({from: OWNER})
+		await t_sale.stopSale({from: OWNER})
 		
 		// Bob tries to buy tickets, but the sale has stopped
 		/*assert*/await tryCatch(
-			ticketsale.buyTicket( 1, {value: 1e+18, from: BOB}), errTypes.revert
+			t_sale.buyTicket( 1, {value: 1e+18, from: BOB}), errTypes.revert
 		)
 
 		// Sale starts again and tickets are sold out
-		await ticketsale.startSale({from: OWNER})
+		await t_sale.startSale({from: OWNER})
 		let soldTickets
 		async function checkTickets(n) {
-			soldTickets = await ticketsale.soldTickets.call()
+			soldTickets = await t_sale.soldTickets()
 			assert.equal(soldTickets.toNumber(), n, 'incorrect number of sold tickets, check buyTicket method')
 		} 
 		await checkTickets(2)
-		await ticketsale.buyTicket( 6, {value: 6e+18, from: ALICE}) // 8 tickets sold
+		await t_sale.buyTicket( 6, {value: 6e+18, from: ALICE}) // 8 tickets sold
 		await checkTickets(8)
-		await ticketsale.buyTicket( 8, {value: 8e+18, from: accounts[3]}) // 16
+		await t_sale.buyTicket( 8, {value: 8e+18, from: accounts[3]}) // 16
 		await checkTickets(16)
-		await ticketsale.buyTicket( 8, {value: 8e+18, from: accounts[4]}) // 24
+		await t_sale.buyTicket( 8, {value: 8e+18, from: accounts[4]}) // 24
 		await checkTickets(24)
-		await ticketsale.buyTicket( 8, {value: 8e+18, from: accounts[5]}) // 32
+		await t_sale.buyTicket( 8, {value: 8e+18, from: accounts[5]}) // 32
 		await checkTickets(32)
-		await ticketsale.buyTicket( 8, {value: 8e+18, from: accounts[6]}) // 40
+		await t_sale.buyTicket( 8, {value: 8e+18, from: accounts[6]}) // 40
 		await checkTickets(40)
-		await ticketsale.buyTicket( 8, {value: 8e+18, from: accounts[7]}) // 48
+		await t_sale.buyTicket( 8, {value: 8e+18, from: accounts[7]}) // 48
 		await checkTickets(48)
-		await ticketsale.buyTicket( 2, {value: 2e+18, from: accounts[8]}) // 50
+		await t_sale.buyTicket( 2, {value: 2e+18, from: accounts[8]}) // 50 => max tickets sold
 		await checkTickets(50)
 
 		// Bob tries to buy tickets, but they are sold out
 		/*assert*/await tryCatch(
-			ticketsale.buyTicket( 5, {value: 5e+18, from: BOB}), errTypes.revert
+			t_sale.buyTicket( 5, {value: 5e+18, from: BOB}), errTypes.revert
 		)
 
 		// Owner adds more tickets
-		await ticketsale.addMoreTickets(5, {from: OWNER})
-		await ticketsale.startSale({from: OWNER})
+		await t_sale.addMoreTickets(5, {from: OWNER})
+		await t_sale.startSale({from: OWNER})
 
 		// Bob can now buy those tickets
-		await ticketsale.buyTicket( 2, {value: 2e+18, from: BOB})
-		bobBalance = await ticketsale.balanceOf.call(BOB)
-		assert.equal(bobBalance.toNumber(), 2, 'ticket balance incorrect, check buyTicket method');
-	});
+		await t_sale.buyTicket( 2, {value: 2e+18, from: BOB})
+		bobBalance = await t_sale.balanceOf(BOB)
+		assert.equal(bobBalance.toNumber(), 2, 'ticket balance incorrect, check buyTicket method')
+	})
 
 
-});
+})
